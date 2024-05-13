7d1898f5daf399ca038db2e872f07e67d777e3ec
Lucca08 <luccabibianogarcia02novo@gmail.com>
2024-05-13 15:11:19 -0300

    commit inicial

---
+++ src/test/java/com/example/TesteBugBank/TesteBugBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TesteBugBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteBugBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
