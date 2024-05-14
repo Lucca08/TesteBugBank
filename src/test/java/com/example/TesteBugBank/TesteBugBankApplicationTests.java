d545982252c39b3c539fbe3bf2d4bda13650800e
Lucca08 <luccabibianogarcia02novo@gmail.com>
2024-05-14 16:57:18 -0300

    commit para tentar acessar o email

--- src/test/java/com/example/TesteBugBank/TesteBugBankApplicationTests.java
+++ src/test/java/com/example/TesteBugBank/TesteBugBankApplicationTests.java
@@ -1,13 +1,48 @@
 package com.example.TesteBugBank;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TesteBugBankApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Value("${BugBank.endpoint}")
+    private String bugBankEndpoint;
 
+    @Test
+    public void contextLoads() {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\lucca.garcia\\Downloads\\TesteBugBank\\TesteBugBank\\chromedriver.exe");
+	WebDriver driver = new ChromeDriver();        
+        try {
+
+			driver.get(bugBankEndpoint);
+            WebElement botaoRegistrar = driver.findElement(By.xpath("//button[contains(text(), 'Registrar')]"));
+			
+			botaoRegistrar.click();
+
+			WebElement inputEmail = driver.findElement(By.xpath("//input[@placeholder='Informe seu e-mail']"));
+            inputEmail.sendKeys("lucca@gmail.com");
+
+	
+
+		
+		
+
+
+			
+
+		
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {	
+			
+		}
+    }
 }
