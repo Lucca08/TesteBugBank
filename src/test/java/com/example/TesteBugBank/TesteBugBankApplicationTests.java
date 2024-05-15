761166e015c922a30c040b954ff6bece53302b5a
Lucca08 <luccabibianogarcia02novo@gmail.com>
2024-05-15 10:38:33 -0300

    commit finalmente consegui completar o cadastro

--- src/test/java/com/example/TesteBugBank/TesteBugBankApplicationTests.java
+++ src/test/java/com/example/TesteBugBank/TesteBugBankApplicationTests.java
@@ -1,5 +1,7 @@
 package com.example.TesteBugBank;
 
+import java.util.NoSuchElementException;
+
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -16,33 +18,50 @@ class TesteBugBankApplicationTests {
 
     @Test
     public void contextLoads() {
-	System.setProperty("webdriver.chrome.driver", "C:\\Users\\lucca.garcia\\Downloads\\TesteBugBank\\TesteBugBank\\chromedriver.exe");
-	WebDriver driver = new ChromeDriver();        
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\lucca.garcia\\Downloads\\TesteBugBank\\TesteBugBank\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();        
+    
         try {
-
-			driver.get(bugBankEndpoint);
+            driver.get(bugBankEndpoint);
+    
             WebElement botaoRegistrar = driver.findElement(By.xpath("//button[contains(text(), 'Registrar')]"));
-			
-			botaoRegistrar.click();
+            botaoRegistrar.click();
+    
+            if (isRegistrationPage(driver)) {
+                WebElement inputName = driver.findElement(By.cssSelector("input[name='name']"));
+                inputName.sendKeys("Lucca Garcia");
 
-			WebElement inputEmail = driver.findElement(By.xpath("//input[@placeholder='Informe seu e-mail']"));
-            inputEmail.sendKeys("lucca@gmail.com");
+                WebElement inputPassword = driver.findElement(By.xpath("(//input[@name='password'])[2]"));
+                inputPassword.sendKeys("senha123");
 
-	
+    
+                WebElement inputEmail = driver.findElement(By.xpath("(//input[@name='email'])[2]"));
+                inputEmail.sendKeys("lucca@gmail.com");
 
-		
-		
-
-
-			
-
-		
-
-
+    
+                WebElement inputPasswordRegistration = driver.findElement(By.cssSelector("input[name='passwordConfirmation']"));
+                inputPasswordRegistration.sendKeys("senha123");
+    
+            } else {
+                System.out.println("Não estamos na página de registro.");
+            }
         } catch (Exception e) {
             e.printStackTrace();
-        } finally {	
-			
-		}
+        } finally {
+         
+        }
     }
-}
+    
+    // Função para verificar se estamos na página de registro
+    private boolean isRegistrationPage(WebDriver driver) {
+        try {
+            driver.findElement(By.cssSelector("input[name='name']"));
+            driver.findElement(By.cssSelector("input[name='password']"));   
+            driver.findElement(By.cssSelector("input[name='email']"));
+            driver.findElement(By.cssSelector("input[name='passwordConfirmation']"));
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+}    
