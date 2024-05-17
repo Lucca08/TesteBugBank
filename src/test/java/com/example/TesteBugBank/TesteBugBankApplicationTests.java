705b35a78b2140ec7bf8cd2ddb17800e67910b42
Lucca08 <luccabibianogarcia02novo@gmail.com>
2024-05-17 09:09:58 -0300

    commit versão final

--- src/test/java/com/example/TesteBugBank/TesteBugBankApplicationTests.java
+++ src/test/java/com/example/TesteBugBank/TesteBugBankApplicationTests.java
@@ -3,8 +3,6 @@ package com.example.TesteBugBank;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.time.Duration;
-import java.util.NoSuchElementException;
-
 
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -17,6 +15,10 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import io.qameta.allure.Allure;
+import io.qameta.allure.Description;
+import io.qameta.allure.Step;
+
 @SpringBootTest
 class TesteBugBankApplicationTests {
 
@@ -24,148 +26,166 @@ class TesteBugBankApplicationTests {
     private String bugBankEndpoint;
 
     @Test
+    @Description("Teste de fluxo completo de registro, login, transferência e verificação de saldo no BugBank")
     public void contextLoads() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\lucca.garcia\\Downloads\\TesteBugBank\\TesteBugBank\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));  
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         JavascriptExecutor js = (JavascriptExecutor) driver;
-    
+
         try {
             driver.get(bugBankEndpoint);
-    
-            WebElement botaoRegistrar = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(), 'Registrar')]")));
-            botaoRegistrar.click();
+            System.out.println("Acessando a página principal: " + bugBankEndpoint);
 
-            WebElement inputEmail = driver.findElement(By.xpath("(//input[@name='email'])[2]"));
-            inputEmail.sendKeys("lucca@gmail.com");
+            registerUser(driver, wait, js, "lucca@gmail.com", "Lucca Garcia", "senha123", "senha123", true);
+            closeModal(driver, wait);
+            limparCampos(driver);
 
-            WebElement InputNome = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[name='name']")));
-            InputNome.sendKeys("Lucca Garcia");
+            registerUser(driver, wait, js, "luccabibianogarcia@gmail.com", "Nome do Novo Usuário", "senha123", "senha123", false);
+            String[] pegarNumeroDaConta = pegarNumeroDaConta(wait);
+            closeModal(driver, wait);
 
-            WebElement InputSenha = driver.findElement(By.xpath("(//input[@name='password'])[2]"));
-            InputSenha.sendKeys("senha123");
+            loginUser(driver, wait, "lucca@gmail.com", "senha123");
+            fazerTransferencia(driver, wait, js, pegarNumeroDaConta[0], pegarNumeroDaConta[1]);
+            closeModal(driver, wait);
+            voltarDaAreadaTransferencia(wait);
+            verificarSaldo(wait, 500.0);
+            sairDaConta(wait);
 
-            WebElement InputConfirmarSenha = driver.findElement(By.cssSelector("input[name='passwordConfirmation']"));
-            InputConfirmarSenha.sendKeys("senha123");
-
-            WebElement toggleAddBalanceLabel = driver.findElement(By.xpath("//label[@id='toggleAddBalance']"));
-            js.executeScript("arguments[0].click();", toggleAddBalanceLabel);
-
-            WebElement cadastrar = driver.findElement(By.xpath("//button[contains(text(), 'Cadastrar')]"));
-            cadastrar.click();
-
-            WebElement closeButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("btnCloseModal")));
-            closeButton.click();
-
-            botaoRegistrar.click();
-        
-            WebElement inputEmail2 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@name='email'])[2]")));
-            inputEmail2.clear();
-            inputEmail2.sendKeys("luccabibianogarcia@gmail.com");
-
-            WebElement InputNome2 = driver.findElement(By.cssSelector("input[name='name']"));
-            InputNome2.clear();
-            InputNome2.sendKeys("Nome do Novo Usuário");
-
-            WebElement InputSenha2 = driver.findElement(By.xpath("(//input[@name='password'])[2]"));
-            InputSenha2.clear();
-            InputSenha2.sendKeys("senha123");
-
-            WebElement InputConfirmarSenha2 = driver.findElement(By.cssSelector("input[name='passwordConfirmation']"));
-            InputConfirmarSenha2.clear();
-            InputConfirmarSenha2.sendKeys("senha123");
-
-            WebElement cadastrar2 = driver.findElement(By.xpath("//button[contains(text(), 'Cadastrar')]"));
-            cadastrar2.click();
-
-            WebElement modalTextElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("modalText")));
-            String mensagemDeSucesso = modalTextElement.getText();
-
-            String[] partes = mensagemDeSucesso.split(" ");
-            String infoConta = partes[2]; // "873-0"
-            String[] partesConta = infoConta.split("-");
-            String numeroConta = partesConta[0];//873
-            String digitoConta = partesConta[1];//0
-
-            
-            WebElement closeButton2 = driver.findElement(By.cssSelector("div.styles__ContainerCloseButton-sc-8zteav-2.ffzYTz a"));
-            js.executeScript("arguments[0].click();", closeButton2);
-
-            WebElement inputEmailLogin = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("email")));
-            inputEmailLogin.sendKeys("lucca@gmail.com");
-
-            WebElement inputSenhaLogin =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("password")));
-            inputSenhaLogin.sendKeys("senha123");
-
-            WebElement acessarButton2 = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.style__ContainerButton-sc-1wsixal-0")));
-            acessarButton2.click();
-
-            WebElement transferencia = wait.until(ExpectedConditions.elementToBeClickable(By.id("btn-TRANSFERÊNCIA")));
-            transferencia.click();
-
-            WebElement InputNumeroDaConta = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("accountNumber")));
-            InputNumeroDaConta.sendKeys(numeroConta);
-
-            WebElement InputDigito = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("digit")));
-            InputDigito.sendKeys(digitoConta);
-
-            WebElement InputValorTransferencia = driver.findElement(By.name("transferValue"));
-            InputValorTransferencia.sendKeys("500");
-
-            WebElement InputDescricao = driver.findElement(By.name("description"));
-            InputDescricao.sendKeys("Transferência de 500 reais");
-
-            WebElement transferirButton = driver.findElement(By.xpath("//button[contains(text(), 'Transferir agora')]"));
-            transferirButton.click();
-
-            WebElement closeButton3 = wait.until(ExpectedConditions.elementToBeClickable(By.id("btnCloseModal")));
-            closeButton3.click();
-
-            WebElement SairButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("btnExit")));
-            SairButton.click();
-
-            WebElement inputEmailLogin2 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("email")));
-            inputEmailLogin2.sendKeys("luccabibianogarcia@gmail.com");
-
-            WebElement inputSenhaLogin2 =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("password")));
-            inputSenhaLogin2.sendKeys("senha123");
-
-            WebElement acessarButton3 = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.style__ContainerButton-sc-1wsixal-0")));
-            acessarButton3.click();
-
-            WebElement saldo = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("textBalance")));
-            String saldoTexto = saldo.getText();
-
-            String saldoValorTexto = saldoTexto.split("R\\$")[1].trim().replace(".", "").replace(",", ".");
-            double saldoValor = Double.parseDouble(saldoValorTexto);
-
-            assertEquals(1500.00, saldoValor, 0.01, "O saldo após a transferência deve ser R$ 1.500,00");
-
-            WebElement SairButton2 = wait.until(ExpectedConditions.elementToBeClickable(By.id("btnExit")));
-            js.executeScript("arguments[0].click();", SairButton2);
-
-            WebElement inputEmailLogin3 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("email")));
-            inputEmailLogin3.sendKeys("lucca@gmail.com");
-
-            WebElement inputSenhaLogin3 =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("password")));
-            inputSenhaLogin3.sendKeys("senha123");
-
-            WebElement acessarButton4 = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.style__ContainerButton-sc-1wsixal-0")));
-            acessarButton4.click();
-
-            WebElement saldo2 = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("textBalance")));
-            String saldoTexto2 = saldo2.getText();
-
-            String saldoValorTexto2 = saldoTexto2.split("R\\$")[1].trim().replace(".", "").replace(",", ".");
-            double saldoValor2 = Double.parseDouble(saldoValorTexto2);
-
-            assertEquals(500.00, saldoValor2, 0.01, "O saldo após a transferência deve ser R$ 500,00");
+            loginUser(driver, wait, "luccabibianogarcia@gmail.com", "senha123");
+            verificarSaldo(wait, 1500.0);
+            sairDaConta(wait);
 
 
-        } catch (NoSuchElementException e) {
-            System.out.println("Elemento não encontrado: " + e.getMessage());
+
+
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
         } finally {
-        
         }
     }
+
+    @Step("Registrar usuário com email: {0}, nome: {1}, senha: {2}, confirmar senha: {3}, adicionar saldo: {4}")
+    private void registerUser(WebDriver driver, WebDriverWait wait, JavascriptExecutor js, String email, String nome, String senha, String confirmarSenha, boolean addBalance) {
+        WebElement botaoRegistrar = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(), 'Registrar')]")));
+        botaoRegistrar.click();
+
+        WebElement inputEmail = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@name='email'])[2]")));
+        inputEmail.sendKeys(email);
+
+        WebElement inputNome = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[name='name']")));
+        inputNome.sendKeys(nome);
+
+        WebElement inputSenha = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//input[@name='password'])[2]")));
+        inputSenha.sendKeys(senha);
+
+        WebElement inputConfirmarSenha = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input[name='passwordConfirmation']")));
+        inputConfirmarSenha.sendKeys(confirmarSenha);
+       
+        WebElement toggleAddBalanceLabel = driver.findElement(By.xpath("//label[@id='toggleAddBalance']"));
+        boolean isToggleActive = toggleAddBalanceLabel.getAttribute("class").contains("active");
+
+        if (addBalance && !isToggleActive) {
+            js.executeScript("arguments[0].click();", toggleAddBalanceLabel);
+        } else if (!addBalance && isToggleActive) {
+            js.executeScript("arguments[0].click();", toggleAddBalanceLabel);
+        }
+
+
+        WebElement cadastrar = driver.findElement(By.xpath("//button[contains(text(), 'Cadastrar')]"));
+        cadastrar.click();
+    }
+
+    @Step("Fechar modal")
+    private void closeModal(WebDriver driver, WebDriverWait wait) {
+        try {
+            WebElement closeButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("btnCloseModal")));
+            closeButton.click();
+            System.out.println("Modal fechado com sucesso.");
+        } catch (Exception e) {
+            System.out.println("Erro ao fechar modal: " + e.getMessage());
+        }
+    }
+
+    @Step("Pegar número da conta")
+    private String[] pegarNumeroDaConta(WebDriverWait wait) {
+        WebElement modalTextElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("modalText")));
+        String mensagemDeSucesso = modalTextElement.getText();
+        String[] partes = mensagemDeSucesso.split(" ");
+        String infoConta = partes[2]; // "873-0"
+        String[] partesConta = infoConta.split("-");
+        return partesConta; // Retorna ["873", "0"]
+    }
+
+    @Step("Logar usuário com email: {0}")
+    private void loginUser(WebDriver driver, WebDriverWait wait, String email, String senha) {
+        WebElement inputEmailLogin = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("email")));
+        inputEmailLogin.sendKeys(email);
+
+        WebElement inputSenhaLogin =  wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("password")));
+        inputSenhaLogin.sendKeys(senha);
+
+        WebElement acessarButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.style__ContainerButton-sc-1wsixal-0")));
+        acessarButton.click();
+    }
+
+    @Step("Limpar campos")
+    private void limparCampos(WebDriver driver) {
+        WebElement inputEmail = driver.findElement(By.xpath("(//input[@name='email'])[2]"));
+        inputEmail.clear();
+
+        WebElement inputNome = driver.findElement(By.cssSelector("input[name='name']"));
+        inputNome.clear();
+
+        WebElement inputSenha = driver.findElement(By.xpath("(//input[@name='password'])[2]"));
+        inputSenha.clear();
+
+        WebElement inputConfirmarSenha = driver.findElement(By.cssSelector("input[name='passwordConfirmation']"));
+        inputConfirmarSenha.clear();
+    }
+
+    @Step("Fazer transferência para conta: {0}-{1}")
+    private void fazerTransferencia(WebDriver driver, WebDriverWait wait, JavascriptExecutor js, String numeroConta, String digitoConta) {
+        WebElement transferencia = wait.until(ExpectedConditions.elementToBeClickable(By.id("btn-TRANSFERÊNCIA")));
+        transferencia.click();
+
+        WebElement inputNumeroDaConta = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("accountNumber")));
+        inputNumeroDaConta.sendKeys(numeroConta);
+
+        WebElement inputDigito = wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("digit")));
+        inputDigito.sendKeys(digitoConta);
+
+        WebElement inputValorTransferencia = driver.findElement(By.name("transferValue"));
+        inputValorTransferencia.sendKeys("500");
+
+        WebElement inputDescricao = driver.findElement(By.name("description"));
+        inputDescricao.sendKeys("Transferência de 500 reais");
+
+        WebElement transferirButton = driver.findElement(By.xpath("//button[contains(text(), 'Transferir agora')]"));
+        transferirButton.click();
+    }
+
+    @Step("Sair da conta")
+    private void sairDaConta(WebDriverWait wait) {
+        WebElement SairButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("btnExit")));
+        SairButton.click();
+    }
+
+    @Step("Voltar da área de transferência")
+    private void voltarDaAreadaTransferencia(WebDriverWait wait) {
+        WebElement voltarButton = wait.until(ExpectedConditions.elementToBeClickable(By.id("btnBack")));
+        voltarButton.click();
+    }
+
+
+    @Step("Verificar saldo: {0}")
+    private void verificarSaldo(WebDriverWait wait, double saldoEsperado) {
+        WebElement saldo = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("textBalance")));
+        String saldoTexto = saldo.getText();
+        String saldoValorTexto = saldoTexto.split("R\\$")[1].trim().replace(".", "").replace(",", ".");
+        double saldoValor = Double.parseDouble(saldoValorTexto);
+        assertEquals(saldoEsperado, saldoValor, 0.01, "O saldo após a transferência deve ser R$ " + saldoEsperado);
+    }
+
+    
 }
